bd8ac0537a5fd7884e5f15f975a8adc8003f965d
Afan Alfi Andi <alfiandiafan@gmail.com>
2025-05-07 23:10:37 +0700

    add: logout

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { AuthService } from "../../../pages/auth/auth.service";
 
 @Component({
-  selector: 'app-navbar',
+  selector: "app-navbar",
   imports: [],
-  templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  templateUrl: "./navbar.component.html",
+  styleUrl: "./navbar.component.css",
 })
 export class NavbarComponent {
-
+  constructor(private authService: AuthService) {}
+  onLogout() {
+    this.authService.logout();
+  }
 }
